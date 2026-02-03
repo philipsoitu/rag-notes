1672da73a81d7f7487ec88be2c028bf42c581d58
philipsoitu <philip.soitu@gmail.com>
2026-02-02 20:28:48 -0500

    flake init

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "yo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.bun
+          ];
+
+          shellHook = ''
+            echo "Bun $(bun --version) ready"
+          '';
+        };
+      }
+    );
+}
